d359485fdd32b181171dc26c03be8e9084935056
Ahmed Nasser <ahmed_fcih@outlook.com>
2020-09-30 22:54:22 +0200

    Added test APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import(
+    APIView
+)
+from rest_framework.response import Response
 
 # Create your views here.
+
+class TestAPIView(APIView):
+    """test API View"""
+
+    def get(self, request, formate=None):
+        """Return a list of APIView features"""
+        
+
+        return Response({'message': 'Hello'})
